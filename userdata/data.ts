8db8e5b719c5ff73288eddeb1cb9a793bec838ef
Vishal101022 <raivishal939@gmail.com>
2023-10-23 23:56:32 +0530

    :tada: user-data

---
+++ userdata/data.ts
@@ -0,0 +1,40 @@
+const userData = {
+  githubUsername: "",
+  name: "",
+  designation: "",
+  avatarUrl: "",
+  email: "",
+  phone: "",
+  address: "",
+  projects: [
+    {
+      title: "",
+      link: "",
+      imgUrl: "",
+      techStack: "",
+    },
+  ],
+  about: {
+    title: "",
+    description: [],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "",
+      company: "",
+      year: "",
+      companyLink: "",
+      desc: "",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    twitter: "",
+    linkedin: "",
+    github: "",
+  },
+};
+
+export default userData;
