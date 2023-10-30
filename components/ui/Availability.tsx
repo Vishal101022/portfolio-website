3bee94c6369a6c8526e120480a8d58c5f1c16813
Vishal101022 <raivishal939@gmail.com>
2023-10-30 23:56:26 +0530

    Revert
    This reverts commit 26cc02b1d40a5277294bb0dc6ee5c63775bf6afc.

--- components/ui/Availability.tsx
+++ components/ui/Availability.tsx
@@ -1,23 +1,9 @@
 // availability component in React
 
-export const AvailabilityStatus = ({
-  isAvailable,
-}: {
-  isAvailable: boolean;
-}) => {
+export const AvailabilityStatus = ({isAvailable}: {isAvailable:boolean}) => {
   return (
     <div>
-      {isAvailable ? (
-        <span className="flex   text-sm items-center space-x-2 bg-green-200/40 px-2 py-1 border rounded-md">
-          <i className="inline h-2 w-2 rounded-full bg-green-500"></i>
-          <span className="text-green-500">Available for Work</span>
-        </span>
-      ) : (
-        <span className="flex  items-center text-sm space-x-2 bg-red-200/40 px-2 py-1 border rounded-md">
-          <i className="inline h-2 w-2 rounded-full bg-red-500"></i>
-          <span className="text-red-500">Not Available</span>
-        </span>
-      )}
+       <p>{isAvailable ? "Available for Work" : "Not Available"}</p>
     </div>
-  );
-};
+  )
+}
