d08f1f96f83ab0423815e72108c4ff58b3dc1b02
Vishal101022 <raivishal939@gmail.com>
2023-10-31 06:39:05 +0530

    :recycle: clean: reduce redundancy

--- components/ui/Availability.tsx
+++ components/ui/Availability.tsx
@@ -5,19 +5,18 @@ export const AvailabilityStatus = ({
 }: {
   isAvailable: boolean;
 }) => {
+  const status = isAvailable ? "Available for Work" : "Not Available";
+  const color = isAvailable ? "green" : "red";
+
   return (
     <div>
-      {isAvailable ? (
-        <span className="flex   text-sm items-center space-x-2 bg-green-200/40 px-2 py-1 border rounded-md">
-          <i className="inline h-2 w-2 rounded-full bg-green-500"></i>
-          <span className="text-green-500">Available for Work</span>
-        </span>
-      ) : (
-        <span className="flex  items-center text-sm space-x-2 bg-red-200/40 px-2 py-1 border rounded-md">
-          <i className="inline h-2 w-2 rounded-full bg-red-500"></i>
-          <span className="text-red-500">Not Available</span>
-        </span>
-      )}
+      <span
+        className={`flex items-center text-sm space-x-2 
+        bg-${color}-200/40 px-2 py-1 border rounded-md`}
+      >
+        <i className={`inline h-2 w-2 rounded-full bg-${color}-500`}></i>
+        <span className={`text-${color}-500`}>{status}</span>
+      </span>
     </div>
   );
 };
