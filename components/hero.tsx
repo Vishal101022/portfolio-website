3f826bf66fa713658d171281faab820b6301a5f9
Vishal Rai <raivishal939@gmail.com>
2023-12-17 23:56:38 +0530

    add: avtar

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,10 @@
 import userData from "@/userdata/data";
 import Button from "./ui/button";
+import Image from "next/image";
 export default function Hero() {
   return (
     <>
-      <section className="max-w-6xl  mx-auto px-4 py-10 ">
+      <section className="max-w-6xl  mx-auto px-4 py-10 flex space-x-24">
         <div>
           <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
             Hey there! I&apos;m <span>{userData.about.title}</span>...
@@ -11,6 +12,15 @@ export default function Hero() {
           <p>{userData.about.description}</p>
           <Button text={"More about Me"} />
           <Button text={"Resume"} />
+        </div>
+        <div className="w-1/3 hidden md:block">
+          <Image
+            src="/images/avtar.jpg"
+            alt="Vishal Kumar"
+            width={150}
+            height={150}
+            className="rounded-md"
+          />
         </div>
       </section>
     </>
