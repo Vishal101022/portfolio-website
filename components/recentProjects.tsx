b2612abd06b59f04a3eae753999d4f855a72e144
Vishal Rai <raivishal939@gmail.com>
2023-12-21 22:18:43 +0530

    :tada: initial release

---
+++ components/recentProjects.tsx
@@ -0,0 +1,14 @@
+export default function RecentProjects() {
+  return (
+    <>
+      <section className="py-10 ">
+        <h1 className="text-4xl font-bold pb-5 text-gray-800 dark:text-gray-200">Recent Projects</h1>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+        </div>
+      </section>
+    </>
+  );
+}
