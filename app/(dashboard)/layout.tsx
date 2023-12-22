522e66dc08c0253ac2a0f2b9351d88a6c27cedea
Vishal Rai <raivishal939@gmail.com>
2023-12-23 02:18:13 +0530

    add: comman style in layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer";
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
     // comman layout that will be used in all pages
-    <section className="dark:bg-gray-800 w-full">
+    <section className="max-w-6xl  mx-auto px-4  dark:bg-gray-800 w-full">
       <Navbar />
       <main>{children}</main>
       <Footer />
