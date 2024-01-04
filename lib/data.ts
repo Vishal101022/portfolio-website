6a5b3abad2593a23ac2095b55a6ed2c32df62816
Vishal101022 <raivishal939@gmail.com>
2024-01-04 23:22:11 +0530

    :card_file_box: update data

---
+++ lib/data.ts
@@ -0,0 +1,50 @@
+export const userData = {
+  githubUsername: "",
+  name: "Vishal Kumar",
+  title: "Make anything possible with Web Development",
+  description:
+    "I'm a passionate web developer dedicated to crafting stunning user experiences on the front-end.",
+  designation: "Web Developer",
+  availability: true,
+  avatarUrl: "",
+  email: "",
+  phone: "",
+  address: "",
+
+  about: {},
+  experience: [
+    {
+      title: "",
+      company: "",
+      year: "",
+      companyLink: "",
+      desc: "",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    twitter: "",
+    linkedin: "",
+    github: "",
+  },
+} as const;
+
+export const projects = [
+  {
+    id: 1,
+    title: "SkillBlend-LMS",
+    description:
+      "A full stack web application that helps users to create and manage courses, access learning materials, track progress on their dashboard,making it a valuable tool for educators and learners.",
+    imgUrl: "/images/dark.jpg",
+    techStack: [
+      { name: "React" },
+      { name: "Tailwind" },
+      { name: "NextJs" },
+      { name: "MongoDB" },
+      { name: "Stripe" },
+      { name: "Prisma" },
+    ],
+    link: "https://skill-blend-lms.vercel.app/",
+    github: "https://github.com/Vishal101022/SkillBlend-LMS",
+  },
+] as const;
