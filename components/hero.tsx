f21e25f135355691704e07e6b95ff44b83f8752e
Vishal101022 <raivishal939@gmail.com>
2024-01-04 23:46:15 +0530

    add: url to button

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import userData from "@/userdata/data";
+import { userData } from "@/lib/data";
 import Button from "./ui/button";
 import WaterDropGrid from "./waterDropGrid";
 export default function Hero() {
@@ -8,15 +8,15 @@ export default function Hero() {
         <div className="py-5 px-2 overflow-hidden dark:bg-gray-900">
           <div className="relative z-10">
             <h1 className="text-4xl  font-bold text-gray-800 dark:text-gray-200">
-              {userData.about.title}
+              {userData.title}
             </h1>
             <p className="text-3xl  py-5 max-w-3xl">
-              {userData.about.description}
+              {userData.description}
             </p>
 
-            <Button text={"More about Me"} />
+            <Button text={"More about Me"} url ={"/about"} />
             <span className="mx-2"></span>
-            <Button text={"Resume"} />
+            <Button text={"Resume"} url={"#"} />
           </div>
           <div className=" absolute z-0 top-[140px] right-[15%] overflow-hidden">
             <WaterDropGrid />
