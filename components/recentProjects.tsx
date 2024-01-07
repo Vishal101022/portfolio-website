14e44f7336133b36e600fd0aae3416d896b7431b
Vishal101022 <raivishal939@gmail.com>
2024-01-08 04:52:47 +0530

    add: project card

--- components/recentProjects.tsx
+++ components/recentProjects.tsx
@@ -1,15 +1,31 @@
-import Card from "./ui/card";
+import Card from "./ui/projectCard";
 
 export default function RecentProjects() {
   return (
     <>
       <section className="md:mt-52">
-        <h1 className="text-4xl font-bold pb-5 text-gray-800 dark:text-gray-200">Recent Projects</h1>
+        <h1 className="text-4xl font-bold pb-5 text-gray-800 dark:text-gray-200">
+          Recent Projects
+        </h1>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-          <div className=" ">
-            <Card/>
+          <div>
+            <Card
+              title="SkillBlend-LMS"
+              description="A full stack web application that helps users to create and manage courses, access learning materials, track progress on their dashboard,making it a valuable tool for educators and learners."
+              imgUrl="/images/p1.png"
+              link="https://skill-blend-lms.vercel.app/"
+              github="https://github.com/Vishal101022/SkillBlend-LMS"
+              techStack={[
+                "React",
+                "Tailwind",
+                "NextJs",
+                "MongoDB",
+                "Stripe",
+                "Prisma",
+              ]}
+            />
           </div>
-          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div></div>
           <div className="h-32 rounded-lg bg-gray-200"></div>
         </div>
       </section>
