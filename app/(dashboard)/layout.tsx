5bc87de8ba3c0d9ef85f16278ed76252039d472e
Vishal Rai <raivishal939@gmail.com>
2024-01-23 23:28:52 +0530

    :iphone: fix spacing

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
-    // comman layout that will be used in all pages
-    <section className="max-w-6xl  mx-auto px-4  dark:bg-gray-800 w-full">
+    <section className="max-w-screen-xl self-stretch m-auto px-6 sm:px-12 py-6 dark:bg-gray-800 w-full">
       <Navbar />
       <main>{children}</main>
       <Footer />
