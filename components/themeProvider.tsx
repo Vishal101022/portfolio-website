aa081dff49c73202ce90ffaf97a29b39623dd9f3
Vishal101022 <raivishal939@gmail.com>
2024-01-28 22:41:42 +0530

    fix: conflict

--- components/themeProvider.tsx
+++ components/themeProvider.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "next-themes";
 
-export function themeProvider({ children }: { children: React.ReactNode }) {
+export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
