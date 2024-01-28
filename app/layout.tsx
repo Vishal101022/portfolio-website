f792cad3dae62aa85bd5bf5038e8d72a91f80df9
Vishal101022 <raivishal939@gmail.com>
2024-01-28 23:29:22 +0530

    add: <Provider>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next'
-import { Roboto_Flex } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Roboto_Flex } from "next/font/google";
+import "./globals.css";
+import { Provider } from "@/components/themeProvider";
 
-const roboto = Roboto_Flex({ subsets: ['latin'] })
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Vishal Kumar",
@@ -12,11 +13,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
-  )
+  );
 }
