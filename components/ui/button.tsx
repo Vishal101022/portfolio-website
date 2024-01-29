e9a04fc18de34f5697614237f8960b59afe767f2
Vishal101022 <raivishal939@gmail.com>
2024-01-30 00:22:32 +0530

    set: dark color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ export default function Button({ text, url }: { text: string; url: string }) {
     <Link href={url}>
       <button
         type="button"
-        className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+        className="py-3 px-4 inline-flex justify-center items-center gap-2 backdrop-blur transition rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none text-sm dark:bg-zinc-900 dark:hover:border-white/20 dark:border-rose-500 dark:text-rose-500 "
       >
         {text}
       </button>
