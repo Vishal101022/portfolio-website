bf6261101fa612b69d7e4c94752604b41f826560
Vishal101022 <raivishal939@gmail.com>
2024-01-31 04:28:17 +0530

    fix: color issue

--- components/ui/projectCard.tsx
+++ components/ui/projectCard.tsx
@@ -20,8 +20,8 @@ const Card = ({
   return (
     <>
       <article>
-        <div className=" hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
-          <div className="w-full h-[430px] overflow-hidden flex flex-col gap-4 rounded-[10px] bg-white p-4 ">
+        <div className="rounded-xl  shadow-sm  bg-gradient-to-b from-zinc-300  to-rose-300  dark:from-zinc-600 dark:to-rose-600 p-0.5 transition hover:scale-105  ">
+          <div className="w-full h-[430px] overflow-hidden flex flex-col gap-4 rounded-[10px] bg-white dark:bg-zinc-800 p-4">
             <div className="flex justify-between">
               {/* title */}
               <h1 className="font-medium">{title}</h1>
@@ -52,14 +52,14 @@ const Card = ({
             </div>
             {/* description */}
             <Link href={link} target="_blank">
-              <h3 className="mt-0.5 text-sm  text-gray-900">{description}</h3>
+              <h3 className="mt-0.5 text-sm  text-gray-700 dark:text-white">{description}</h3>
             </Link>
             {/* techStack */}
-            <div className="mt-4 flex flex-wrap gap-1">
+            <div className="mt-4 flex flex-wrap gap-2">
               {techStack.map((tech, index) => (
                 <span
                   key={index}
-                  className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+                  className="whitespace-nowrap rounded-sm font-light italic bg-slate-100 px-2.5 py-0.5 text-xs text:grey-500 dark:bg-zinc-800 dark:ring-[0.5px] dark:ring-zinc-500 dark:text-white"
                 >
                   {tech}
                 </span>
