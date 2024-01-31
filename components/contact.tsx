7de5ff8a21d7d332b58b9bc5cc5da31f095d8237
Vishal101022 <raivishal939@gmail.com>
2024-01-31 23:26:21 +0530

    :lipstick: style color

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { userData } from "@/lib/data";
 import { CiMail } from "react-icons/ci";
+import Divider from "./ui/divider";
 
 function Contact() {
   const whatsappLink: string =
     "https://wa.me/7004575424?text=Hi Vishal, Please send your resume to me.";
   return (
     <>
-      <section className="my-10 py-10">
+      <section className="my-10 ">
         {/* contact card */}
-        <div className="border border-blue-200 rounded p-6 my-4 w-full lg:w-2/3 mx-auto dark:border-gray-800 bg-blue-50 dark:bg-blue-opaque">
+        <div className="border border-rose-200  rounded p-6 my-4 w-full  dark:border-rose-600 bg-gray-50 dark:bg-zinc-800">
           <h5 className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
             Drop your message
           </h5>
@@ -17,12 +18,12 @@ function Contact() {
             It won&apos;t take more than 10 seconds..
           </p>
           <Link href={whatsappLink} target="_blank">
-            <div className="mt-4 px-4 text-center w-full font-bold h-8 hover:bg-green-500 bg-green-400 dark:bg-green-400 text-gray-900 py-1 dark:text-gray-900 rounded">
+            <div className="mt-4 px-4 text-center w-full font-bold h-8 hover:bg-green-500 bg-green-400  text-gray-900 py-1 dark:text-zinc-800 rounded">
               Chat on WhatsApp
             </div>
           </Link>
           {/* divider */}
-          <div className="border-t-2 my-8 border-gray-300"></div>
+          <Divider margin={8}/>
           {/* email */}
           <div>
             <p className="my-1 text-gray-800 dark:text-gray-200">
