27895f5d28d8fa73a493fd9cce7b749482fb8f3b
Vishal101022 <raivishal939@gmail.com>
2024-01-31 05:44:22 +0530

    set: props

--- components/ui/divider.tsx
+++ components/ui/divider.tsx
@@ -1,3 +1,20 @@
-export default function Divider() {
-  return <div className="h-0.5 w-full bg-zinc-300 dark:bg-zinc-700"></div>;
+export default function Divider({ margin }: { margin: number }) {
+    // this varible is for dynamic color in tailwind
+    const possible = [
+        "my-1",
+        "my-2",
+        "my-3",
+        "my-4",
+        "my-5",
+        "my-6",
+        "my-7",
+        "my-8",
+        "my-9",
+        "my-10",
+    ]
+  return (
+    <div
+      className={`my-${margin} h-0.5 w-full bg-zinc-300 dark:bg-zinc-700`}
+    ></div>
+  );
 }
