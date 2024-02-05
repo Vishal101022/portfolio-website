a4ae7fb14bf01506079b01ace1ab923426d67f21
Vishal Rai <raivishal939@gmail.com>
2024-02-05 23:16:30 +0530

    :card_file_box: feat: array of string

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,7 @@
 export const userData = {
   name: "Vishal Kumar",
   title: "Hi there, I’m Vishal.",
-  description:
-    "I’m a web developer who loves building things with code.",
+  description: "I’m a web developer who loves building things with code.",
   designation: "Web Developer",
   availability: true,
   avatarUrl: "/images/avtar.jpg",
@@ -31,14 +30,7 @@ export const projects = [
     description:
       "A full stack web application that helps users to create and manage courses, access learning materials, track progress on their dashboard,making it a valuable tool for educators and learners.",
     imgUrl: "/images/p1.png",
-    techStack: [
-      { name: "React" },
-      { name: "Tailwind" },
-      { name: "NextJs" },
-      { name: "MongoDB" },
-      { name: "Stripe" },
-      { name: "Prisma" },
-    ],
+    techStack: ["React", "Tailwind", "NextJs", "MongoDB", "Stripe", "Prisma"],
     link: "https://skill-blend-lms.vercel.app/",
     github: "https://github.com/Vishal101022/SkillBlend-LMS",
   },
@@ -48,12 +40,7 @@ export const projects = [
     description:
       "sleek and minimalistic website with a harmonious color palette",
     imgUrl: "/images/p2.png",
-    techStack: [
-      { name: "Html" },
-      { name: "Css" },
-      { name: "JavaScript" },
-      { name: "Bootstrap" },
-    ],
+    techStack: ["Html", "Css", "JavaScript", "Bootstrap"],
     link: "https://rahulkathak.com/",
     github: "https://github.com/Vishal101022/",
   },
