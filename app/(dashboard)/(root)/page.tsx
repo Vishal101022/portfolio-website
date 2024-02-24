abc51bb61998a0ea64171e4d5c6e0a86cb6d08db
Vishal Rai <raivishal939@gmail.com>
2024-02-24 23:47:56 +0530

    add: latestCode component

--- app/(dashboard)/(root)/page.tsx
+++ app/(dashboard)/(root)/page.tsx
@@ -2,12 +2,14 @@ import Hero from "@/components/hero";
 import RecentProjects from "@/components/recentProjects";
 import Stack from "@/components/stack";
 import Contact from "@/components/contact";
+import LatestCode from "@/components/latestCode";
 export default function Home() {
   return (
     <div>
       <Hero />
       <RecentProjects />
       <Stack />
+      <LatestCode />
       <Contact />
     </div>
   );
