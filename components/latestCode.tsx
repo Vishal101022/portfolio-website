a98643daacaef023b36626e64f7071d8d1378352
Vishal Rai <raivishal939@gmail.com>
2024-02-29 12:54:49 +0530

    add: manual loading state

--- components/latestCode.tsx
+++ components/latestCode.tsx
@@ -2,7 +2,9 @@
 import { useState, useEffect } from "react";
 import GetRepos from "@/lib/githubRepos";
 import RepoCard from "./ui/repoCard";
-
+import Button from "./ui/button";
+import { userData } from "@/lib/data";
+import Loading from "./ui/loading";
 interface RepoData {
   id: number;
   name: string;
@@ -12,23 +14,43 @@ interface RepoData {
 
 export default function LatestCode() {
   const [latestRepos, setLatestRepos] = useState<RepoData[]>([]);
-
+  /* manual loading state because react suspense doesn't work on useEffect*/
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function fetchData() {
-      const repos = await GetRepos();
-      setLatestRepos(repos);
+      try {
+        const repos = await GetRepos();
+        setLatestRepos(repos);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl  px-10 lg:-mt-10 gap-y-20">
-        {latestRepos &&
-          latestRepos.map((repo) => (
-            <RepoCard key={repo.id} latestRepo={repo} />
-          ))}
-      </div>
+      <section className="py-10">
+        <h1 className="text-4xl font-bold pb-5 text-gray-800 dark:text-gray-200">
+          Latest Code
+        </h1>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 gap-y-20">
+            {latestRepos &&
+              latestRepos.map((repo) => (
+                <RepoCard key={repo.id} latestRepo={repo} />
+              ))}
+          </div>
+        )}
+        <div className="flex justify-center pt-5">
+          {/* button */}
+          <Button text={"View Github"} url={userData.socialLinks.github} />
+        </div>
+      </section>
     </>
   );
 }
