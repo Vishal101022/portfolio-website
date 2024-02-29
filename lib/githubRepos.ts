e539fbe9ecb45ad7fae2f172691e77ef4eece00d
Vishal Rai <raivishal939@gmail.com>
2024-02-29 12:39:18 +0530

    move try-catch block to parent component

--- lib/githubRepos.ts
+++ lib/githubRepos.ts
@@ -14,34 +14,27 @@ interface Repository {
 }
 
 async function GetRepos(): Promise<Repository[]> {
-  console.log("Inside GetRepos function");
-  try {
-    const response = await octokit.request("GET /users/{owner}/repos", {
-      owner: "Vishal101022",
-      headers: {
-        "Accept": "application/vnd.github.v3+json",
-        "X-GitHub-Api-Version": "2022-11-28",
-        
-      },
-    });
+  //console.log("Inside GetRepos function");
+  const response = await octokit.request("GET /users/{owner}/repos", {
+    owner: "Vishal101022",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
 
-    const repos: Repository[] = response.data
-      .map((repo: any) => ({
-        id: repo.id,
-        name: repo.name,
-        description: repo.description,
-        html_url: repo.html_url,
-        pushed_at: repo.pushed_at,
-      }))
-      .filter(
-        (repo: Repository) => new Date(repo.pushed_at) > new Date("2023-11-01")
+  const repos: Repository[] = response.data
+    .map((repo: any) => ({
+      id: repo.id,
+      name: repo.name,
+      description: repo.description,
+      html_url: repo.html_url,
+      pushed_at: repo.pushed_at,
+    }))
+    .filter(
+      (repo: Repository) => new Date(repo.pushed_at) > new Date("2023-11-01")
     );
-    //console.log("Repos fetched successfully:", repos);
-    return repos;
-  } catch (error) {
-    console.error("Error fetching repositories:", error);
-    return [];
-  }
+  return repos;
 }
 
 export default GetRepos;
