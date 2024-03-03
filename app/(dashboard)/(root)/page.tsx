ba85367c19c779c49cd5bb157eb3103b22db805f
Vishal Rai <raivishal939@gmail.com>
2024-03-03 23:41:45 +0530

    add: fragments

--- app/(dashboard)/(root)/page.tsx
+++ app/(dashboard)/(root)/page.tsx
@@ -3,14 +3,15 @@ import RecentProjects from "@/components/recentProjects";
 import Stack from "@/components/stack";
 import Contact from "@/components/contact";
 import LatestCode from "@/components/latestCode";
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
       <RecentProjects />
       <Stack />
       <LatestCode />
       <Contact />
-    </div>
+    </>
   );
 }
