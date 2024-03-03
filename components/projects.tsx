db1005da9174d6f9aa3bf6f9a857bcfc800bd43f
Vishal Rai <raivishal939@gmail.com>
2024-03-03 23:49:12 +0530

    add: page animation

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,32 +1,34 @@
 import React from "react";
 import Card from "./ui/projectCard";
 import { projects } from "@/lib/data";
-
+import { AnimatePage } from "./ui/animatePage";
 export default function Projects() {
   return (
-    <section>
-      <h1 className="text-2xl md:text-4xl font-semibold  text-gray-800 dark:text-gray-200">
-        What I&apos;ve been working on
-      </h1>
-      <p className="text-lg md:text-xl py-5  text-gray-600 dark:text-gray-200">
-        I like to stay busy and always have a project in the works.
-        <br />
-        Take a look at some of the applications I have worked on.
-      </p>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
-        {projects.map((project, index) => (
-          <div key={index}>
-            <Card
-              title={project.title}
-              description={project.description}
-              imgUrl={project.imgUrl}
-              link={project.link}
-              github={project.github}
-              techStack={project.techStack.map((tech) => tech)}
-            />
-          </div>
-        ))}
-      </div>
-    </section>
+    <AnimatePage>
+      <section>
+        <h1 className="text-2xl md:text-4xl font-semibold  text-gray-800 dark:text-gray-200">
+          What I&apos;ve been working on
+        </h1>
+        <p className="text-lg md:text-xl py-5  text-gray-600 dark:text-gray-200">
+          I like to stay busy and always have a project in the works.
+          <br />
+          Take a look at some of the applications I have worked on.
+        </p>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+          {projects.map((project, index) => (
+            <div key={index}>
+              <Card
+                title={project.title}
+                description={project.description}
+                imgUrl={project.imgUrl}
+                link={project.link}
+                github={project.github}
+                techStack={project.techStack.map((tech) => tech)}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    </AnimatePage>
   );
 }
