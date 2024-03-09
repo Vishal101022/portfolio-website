afbb9a0c4c16bcb480e39eb63e102c5bea553497
Vishal Rai <raivishal939@gmail.com>
2024-03-09 21:20:30 +0530

    set: children height to min

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,9 +3,9 @@ import Footer from "@/components/footer";
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-4xl m-auto px-6 sm:px-12 py-6 w-full">
+    <div className="max-w-4xl h-screen m-auto px-6 sm:px-12 py-6 ">
       <Navbar />
-      <main>{children}</main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
     </div>
   );
