479a4e73bc16b1b7ae5ef497558a8632138fa7de
Vishal Rai <raivishal939@gmail.com>
2024-03-10 19:01:14 +0530

    style: added dot background on card

--- components/postPreview.tsx
+++ components/postPreview.tsx
@@ -9,12 +9,20 @@ interface PostMetadata {
 
 function PostPreview({ title, subtitle, date, slug }: PostMetadata) {
   return (
-    <div>
-      <Link href={`/blog/${slug}`}>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-        <p>{date}</p>
-      </Link>
+    <div className="flex justify-between ">
+      <span className="dark:bg-zinc-800 dark:border-zinc-700 rounded-md shadow-sm mb-4 p-4 border z-[-2] border-gray-200 inset-0 h-full w-full bg-white  bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#363440_1px,transparent_1px)] [background-size:16px_16px]">
+        <Link href={`/blog/${slug}`} >
+          <h3 className="text-base md:text-xl font-semibold mb-2 w-full text-gray-900 dark:text-gray-100">
+            {title}
+          </h3>
+          <p className="text-gray-500 text-left md:text-right mb-4 md:mb-0">
+            {date}
+          </p>
+          <p className="text-base text-gray-600 dark:text-gray-400">
+            {subtitle}
+          </p>
+        </Link>
+      </span>
     </div>
   );
 }
