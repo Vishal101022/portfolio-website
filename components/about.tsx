e04bf15e28771874463821d90e762232dc6d6b5c
Vishal Rai <raivishal939@gmail.com>
2024-03-11 17:25:56 +0530

    add: about me article

--- components/about.tsx
+++ components/about.tsx
@@ -5,24 +5,27 @@ import { MdOutlineWorkOutline } from "react-icons/md";
 import { about } from "@/lib/data";
 import SocialLinks from "./socialLinks";
 import { AnimatePage } from "./ui/animatePage";
+
 export default function About() {
   return (
     <>
       <AnimatePage>
-        <div className="flex flex-col gap-8 sm:flex-row ">
+        <div className="flex flex-col items-center gap-8 sm:flex-row ">
           {/* avatar */}
-          <div className="flex flex-col items-center">
-            <div className="relative">
+          <div className=" rounded-full bg-gradient-to-tl from-purple-700/60 to-rose-400/60 shadow-lg p-[4px] ring-[5px] ring-purple-500/10">
+            <div className="rounded-full p-px  w-[94px] h-[auto]">
               <Image
                 src="/images/avtar.jpg"
-                alt="Vishal Kumar"
-                width={150}
-                height={150}
-                className="rounded-sm shadow"
+                alt="Photo of Vishal"
+                width={84}
+                height={0}
+                style={{ width: "100%", height: "auto" }}
+                priority={true}
+                className="rounded-full "
               />
             </div>
           </div>
-          {/* details */}
+
           <div className="flex flex-col gap-2 place-self-center">
             {/* designation */}
             <p className="flex gap-1 text-base font-medium text-slate-500 dark:text-slate-400">
@@ -53,8 +56,51 @@ export default function About() {
             <SocialLinks socialLinks={userData.socialLinks} />
           </div>
         </div>
-
-        <div></div>
+        {/* about me article */}
+        <div className="relative">
+          <article>
+            <p className=" text-base font-medium text-slate-500 dark:text-slate-400 py-4">
+              Hello world! I&apos;m Vishal, a code-loving introvert.
+              <br />I first dipped my toes into{" "}
+              <span className="underline decoration-sky-500">
+                programming back in 9th grade
+              </span>{" "}
+              with Java. from childhood, I&apos;ve been fascinated by tech and
+              always wanted to play around with it. My dad got me a computer and
+              well, I literally screwed it up.
+              <br />
+              <br />I decided I wanted to pursue a graduation degree in computer
+              science. There, I studied C++, DSA, Python, and a bunch of
+              programming languages but became a master of none. Alongside my
+              studies, I also took on freelance work. In 2020, the Corona
+              pandemic disrupted my entire year. At the end of the year,{" "}
+              <span className="underline decoration-sky-500">
+                I decided to pursue MCA
+              </span>
+              <br />
+              <br />
+              At the beginning of the year, I was learning DSA to become a{" "}
+              <span className="underline decoration-wavy decoration-red-500">
+                software engineer{" "}
+              </span>
+              , but I really failed to achieve my goal because I did not enjoy
+              DSA much. During the summer vacation, I took the FreeCodeCamp{" "}
+              <span className="underline decoration-sky-500">
+                Responsive Web Design course
+              </span>
+              . I enjoyed learning it, and that&apos;s when I developed an{" "}
+              <span className="underline decoration-green-500">
+                interest in web development
+              </span>{" "}
+              because I saw great things happening on the screen.
+              <br />
+              <br />
+              Since then, I&apos;ve been honing my skills and learning
+              JavaScript. I&apos;m seeking a full-time role where I can help a
+              company achieve their goals.
+            </p>
+          </article>
+        </div>
       </AnimatePage>
     </>
   );
