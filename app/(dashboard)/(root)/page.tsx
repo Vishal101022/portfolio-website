8b813636133109283cd2917df20c1141a9f72c6d
Vishal Rai <raivishal939@gmail.com>
2024-03-13 10:42:14 +0530

    style: added page animation

--- app/(dashboard)/(root)/page.tsx
+++ app/(dashboard)/(root)/page.tsx
@@ -3,15 +3,18 @@ import RecentProjects from "@/components/recentProjects";
 import Stack from "@/components/stack";
 import Contact from "@/components/contact";
 import LatestCode from "@/components/latestCode";
+import { AnimatePage } from "@/components/ui/animatePage";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <RecentProjects />
-      <Stack />
-      <LatestCode />
-      <Contact />
+      <AnimatePage>
+        <Hero />
+        <RecentProjects />
+        <Stack />
+        <LatestCode />
+        <Contact />
+      </AnimatePage>
     </>
   );
 }
