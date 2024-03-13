c2bb291e6f1099ff8f2a78ee86c9d58330d2f0fa
Vishal Rai <raivishal939@gmail.com>
2024-03-13 20:34:30 +0530

    style: added radial-gradient to top page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer";
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-4xl h-screen m-auto px-6 sm:px-12 py-6 ">
+    <div className="max-w-4xl h-screen m-auto px-6 sm:px-12 py-6  top-0 z-[-2] bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0%,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
       <Navbar />
       <main className="min-h-screen">{children}</main>
       <Footer />
