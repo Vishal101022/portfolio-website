a011fe0d3d9e16154f6aea94c1519b2909146db8
Vishal Rai <raivishal939@gmail.com>
2024-03-23 13:45:40 +0530

    Add: infiniteSlider animation & keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,16 @@ const config: Config = {
     extend: {
       animation: {
         background: "background ease infinite",
+        ["infinite-slider"]: "infiniteSlider 30s ease infinite",
       },
       keyframes: {
         background: {
           "0%, 100%": { backgroundPosition: "0% 50%" },
           "50%": { backgroundPosition: "100% 50%" },
+        },
+        infiniteSlider: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(calc(-250px * 4))" },
         },
       },
     },
