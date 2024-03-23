9818778d3a99669e5364c5cd3ccac6ccb6d10d73
Vishal Rai <raivishal939@gmail.com>
2024-03-23 13:44:51 +0530

    Add: infine slider

---
+++ components/infiniteSlider.tsx
@@ -0,0 +1,106 @@
+"use client";
+import {
+  TbBrandHtml5,
+  TbBrandFramerMotion,
+  TbBrandCss3,
+  TbBrandJavascript,
+  TbBrandReact,
+  TbBrandNextjs,
+  TbBrandMysql,
+  TbBrandTailwind,
+} from "react-icons/tb";
+const LOGOS = [
+  {
+    logo: (
+      <TbBrandHtml5 className="text-6xl text-slate-800 dark:text-slate-300" />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandCss3
+        width={30}
+        height={30}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandJavascript
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandReact
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandNextjs
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandMysql
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandTailwind
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+  {
+    logo: (
+      <TbBrandFramerMotion
+        width={24}
+        height={24}
+        className="text-6xl text-slate-800 dark:text-slate-300"
+      />
+    ),
+  },
+];
+
+export default function InfiniteSlider() {
+  return (
+    <div className="relative m-auto w-full overflow-hidden">
+      <div className="animate-infinite-slider flex w-[calc(250px*10)]">
+        {LOGOS.map((item, index) => (
+          <div
+            className="slide flex w-[125px] items-center justify-center"
+            key={index}
+          >
+            {item.logo}
+          </div>
+        ))}
+        {LOGOS.map((item, index) => (
+          <div
+            className="slide flex w-[125px] items-center justify-center"
+            key={index}
+          >
+            {item.logo}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
