5cc31d42f032573d651238b45592ff58160a09ec
Vishal Rai <raivishal939@gmail.com>
2024-03-25 13:11:45 +0530

    Style: container size

--- components/postPreview.tsx
+++ components/postPreview.tsx
@@ -10,7 +10,7 @@ interface PostMetadata {
 function PostPreview({ title, subtitle, date, slug }: PostMetadata) {
   return (
     <Link href={`/blog/${slug}`}>
-      <div className="flex justify-between max-w-lg mx-auto">
+      <div className="flex justify-between mx-auto">
         <span className="dark:bg-zinc-800 dark:border-zinc-700 rounded-md shadow-sm mb-4 p-4 border z-[-2] border-gray-200 inset-0 h-full w-full bg-white  bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#363440_1px,transparent_1px)] [background-size:16px_16px]">
           <h3 className="text-base md:text-xl font-semibold mb-2 w-full text-gray-900 dark:text-gray-100">
             {title}
